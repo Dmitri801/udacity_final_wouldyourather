471f58d9ab19a8ae68590487aa3c31d410fb546f
Dmitriy Sharshiner <dmi@Dmitriys-Mac-mini.local>
2018-08-24 13:04:04 -0600

    removed Redux Dev Tools for production

--- src/store.js
+++ src/store.js
@@ -1,17 +1,14 @@
 import { createStore, applyMiddleware, compose } from "redux";
 import rootReducer from "./reducers";
 import thunk from "redux-thunk";
-
+const reduxDevTools =
+  "window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()";
 const initialState = {};
 const middleware = [thunk];
 const store = createStore(
   rootReducer,
   initialState,
-  compose(
-    applyMiddleware(...middleware),
-    window.__REDUX_DEVTOOLS_EXTENSION__COMPOSE__ &&
-      window.__REDUX_DEVTOOLS_EXTENSION__()
-  )
+  compose(applyMiddleware(...middleware))
 );
 
 export default store;
